5144fc4e6a15d271d1cdff70f9ab0e9bb2161d68
poojyanth <96917000+poojyanth@users.noreply.github.com>
2023-05-15 11:32:56 +0530

    updated background design

--- index.js
+++ index.js
@@ -43,4 +43,4 @@ app.get('/suggest/:q', (req, res) => {
 
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`app listening on port ${port}!`))
